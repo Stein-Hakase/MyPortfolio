d45f4e43412a46d285ed03f97e8abb0473c93a4a
Stein-Hakase <fakhriddine.hariz@gmail.com>
2020-04-14 13:30:24 +0700

    Updates

---
+++ precache-manifest.6c0ea89f910da9b722abed5f47bd92b7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a74aeea394377c07f47b9e9e28f72e1b",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "a4c9bdb3012a5a3c3644",
+    "url": "/MyPortfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "de9d269465df337f0b03",
+    "url": "/MyPortfolio/static/css/main.1fde787e.chunk.css"
+  },
+  {
+    "revision": "a4c9bdb3012a5a3c3644",
+    "url": "/MyPortfolio/static/js/2.0acd583c.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/MyPortfolio/static/js/2.0acd583c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de9d269465df337f0b03",
+    "url": "/MyPortfolio/static/js/main.a1181025.chunk.js"
+  },
+  {
+    "revision": "5b821521451268ee28ae",
+    "url": "/MyPortfolio/static/js/runtime-main.3e2257a3.js"
+  },
+  {
+    "revision": "2466c69e97723a52f74d775f3c82442a",
+    "url": "/MyPortfolio/static/media/mypic.2466c69e.jpg"
+  }
+]);
